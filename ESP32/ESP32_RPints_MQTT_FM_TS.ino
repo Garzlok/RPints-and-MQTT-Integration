7fda1d5b01746368555507d1a80a35ca741500d4
Eddie G <60665719+Garzlok@users.noreply.github.com>
2026-05-08 19:21:28 -0400

    Update ESP32_RPints_MQTT_FM_TS.ino

--- ESP32/ESP32_RPints_MQTT_FM_TS.ino
+++ ESP32/ESP32_RPints_MQTT_FM_TS.ino
@@ -31,7 +31,7 @@ const char* password = "SSID PW";
 // MQTT Settings
 const char* mqtt_server = "raspberrypints.local";   //If your MQTT_PW has a static IP, you can use the IP address.
 const int mqtt_port = 1883;
-const char* mqtt_user = "MQTT_PW";           //If you change the MQTT user name, make sure you add that name here.
+const char* mqtt_user = "RaspberryPints";           //If you change the MQTT user name, make sure you add that name here.
 const char* mqtt_pass = "MQTT Broker PW";
 
 // Flow Sensor1 Pin (Lolin D32)
